e66f6050761ba806834b59fc9105e2b7e8e38f0a
Max Patten <amanthus2535@gmail.com>
2019-06-21 19:30:14 -0700

    Added non letter input validation and claened up text, including specific log for last guess

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const usedWords = [];
 const divider = "\n --------------------------------------------------- \n";
 const spacer = "\r\n\r\n\r\n";
 const clearText = spacer+spacer+spacer+spacer+spacer+spacer+spacer+spacer+spacer+spacer;
-let word;
-let guesses;
 let won = 0;
 let lost = 0;
+const scoreReport = "You've won " + won + " rounds... \n ...and lost "+ lost+ " rounds." + divider;
+let word;
+let guesses;
+
+
 
 function wordPicker() {
     let randomVal = Math.floor(Math.random() * words.length);
@@ -34,34 +37,47 @@ var round = function () {
     if (guesses < 1) {
         console.log(clearText + divider + "You ran out of guesses, better luck next time!" + divider);
         lost++;
-        console.log("You've won " + won + " rounds... \n ...and lost "+ lost+ " rounds." + divider)
+        console.log("You've won " + won + " round(s)... \n ...and lost "+ lost+ " round(s)." + divider)
         playAgain();
     } else if (trigger) {
         console.log(clearText + divider + "Nicely done! You guessed the word correctly." + divider)
         won++;
-        console.log("You've won " + won + " rounds... \n ...and lost "+ lost+ " rounds." + divider)
+        console.log("You've won " + won + " round(s)... \n ...and lost "+ lost+ " round(s)." + divider)
         playAgain();
     }
     else {
-        // console.log(divider + spacer + word.display() + spacer + divider )
-        console.log(divider + word.display() + divider + "\n  You have " + guesses + " guesses remaining..." + divider)
+        console.log(divider + "Type in 'Quit' to exit or 'Score' to view wins/losses")
+        if (guesses > 1){
+            console.log(divider + word.display() + divider + "\n  You have " + guesses + " incorrect guesses remaining..." + divider)
+        } else {
+            console.log(divider + word.display() + divider + "\n  You have only " + guesses + " wrong guess remaining... Choose wisely!" + divider)
+        }
         inquirer.prompt([
             {
                 type: "input",
                 name: "guess",
-                message: "What letter would you like to guess??? (Or type quit to exit game)"
+                message: "What letter would you like to guess???"
             }
         ]).then(function (user) {
            
             if (user.guess.toLowerCase() === "quit") {
                 console.log(clearText +"Thanks for playing, goodbye!")
                 return;
+            }else if (user.guess.toLowerCase() === "score") {
+                    console.log(clearText + "You've won " + won + " round(s)... \n ...and lost "+ lost+ " round(s).")
+                    round();
+                    return;
             }else if (user.guess.length > 1 || user.guess.length === 0){
                 console.log(clearText +"You typed in " + user.guess.length + " characters")
-                console.log("Please only enter one letter!")
+                console.log("Please only enter exactly one letter!")
                 round();
                 return;
-            } 
+            } else if (user.guess.match(/[^a-zA-Z]/)) {
+                console.log(clearText +"You typed in " + user.guess)
+                console.log("Please only enter one letter (No numbers or symbols)!")
+                round();
+                return;
+            }
             let flag = false;
             word.userGuess(user.guess);
             for (let i = 0; i < word.lettersReference.length; i++){
@@ -70,11 +86,11 @@ var round = function () {
                 }
             }
             if (flag) {
-                console.log(clearText + "You guessed: "+ user.guess.toUpperCase() + divider + "Nice! That was one of the letters");
+                console.log(clearText + "You guessed: "+ user.guess.toUpperCase() + divider + "NICE! That was one of the letters");
                 round();
             } else {
                 guesses--;
-                console.log(clearText + "You guessed: "+ user.guess.toUpperCase() + divider + "Oops, that wasn't one of the letters!");
+                console.log(clearText + "You guessed: "+ user.guess.toUpperCase() + divider + "OOPS, that wasn't one of the letters!");
                 round();
             }
         }).catch(function (err) {
@@ -104,7 +120,7 @@ function playAgain() {
 }
 
 function gamePlay() {
-    console.log(clearText + divider + "Welcome to Constructor-Hangman!\n");
+    console.log(clearText + divider + "Welcome to Constructor-Hangman!");
     word = new Word(wordPicker());
     word.buildWord();
     word.userGuess(" ");
